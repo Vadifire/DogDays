156bfd14c12f696a70ac10efe5c884976fb3a924
Cedric Debelle <cedricdebelle@utexas.edu>
2018-11-22 15:50:13 -0600

    add breeds.csv

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import csv
 from skimage.io import imread
 
+''' Create and train model using training data'''
 def train_model(train_dir, train_labels):
     lst = os.listdir(train_dir)
     lst.sort(); # ensure alpha-numeric for quick lookup
@@ -15,5 +17,17 @@ def train_model(train_dir, train_labels):
         # Train model using image 'im' and label 'breed'
         i=i+1
 
+''' Label images from testing dataset using trained model '''
+def label_images(test_dir, breeds):
+    lst = os.listdir(test_dir)
+    print(breeds)
+    test_labels = pd.DataFrame(columns = breeds)
+    print(test_labels.head())
+    print(test_labels.shape)
+
+with open('breeds.csv', 'r') as f:
+    breeds = list(csv.reader(f, delimiter='\n'))
 train_labels = pd.read_csv("labels.csv")
-train_model("train/", train_labels)
+#train_model("train/", train_labels)
+label_images("test/", breeds)
+
